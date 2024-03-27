ed6fbd32d8808648e8ca298f682ff7383e873d87
AngusRenugounder <161382565+AngusRenugounder@users.noreply.github.com>
2024-03-27 21:20:21 +1300

    Update game.js
    Projectile enemy mechanics are working and are getting removed when the player projectile hits them. Need to add projectile enemies onto waves: 8, 9 and 10 with different shooting intervals.

--- game.js
+++ game.js
@@ -21,14 +21,30 @@ var normalEnemyWaveSpawn = "true";
 var fastEnemyWaveSpawn = "true";
 var normalEnemyWaveSpawnCounter = [];
 
-var enemyProjectileOneCooldown = 0;
-var enemyProjectileDespawn = 0;
+var projectileEnemyOneSpawnCounter = 1;
+
+var enemyProjectileOneCooldown = [];
+enemyProjectileOneCooldown[1] = 0;
+enemyProjectileOneCooldown[2] = 0;
+enemyProjectileOneCooldown[3] = 0;
+enemyProjectileOneCooldown[4] = 0;
+
+var enemyProjectileTwoCooldown = [];
+enemyProjectileTwoCooldown[1] = 0;
+
+var enemyProjectileOneDespawn = [];
+enemyProjectileOneDespawn[1] = 0;
+enemyProjectileOneDespawn[2] = 0;
 
 normalEnemyWaveSpawnCounter[1] = 0;
 normalEnemyWaveSpawnCounter[2] = 0;
 normalEnemyWaveSpawnCounter[3] = 0;
 normalEnemyWaveSpawnCounter[4] = 0;
 normalEnemyWaveSpawnCounter[5] = 0;
+normalEnemyWaveSpawnCounter[6] = 0;
+normalEnemyWaveSpawnCounter[7] = 0;
+normalEnemyWaveSpawnCounter[8] = 0;
+normalEnemyWaveSpawnCounter[9] = 0;
 
 var playerSpawn = 0;
 
@@ -37,8 +53,10 @@ fastEnemyWaveSpawnCounter = [];
 
 fastEnemyWaveSpawnCounter[1] = 0;
 fastEnemyWaveSpawnCounter[2] = 0;
+fastEnemyWaveSpawnCounter[3] = 0;
+fastEnemyWaveSpawnCounter[4] = 0;
+fastEnemyWaveSpawnCounter[5] = 0;
 
-var projectileEnemySpawn = 0;
 
 var fastEnemyCount = 1;
 var lastEnemy = [];
@@ -48,7 +66,8 @@ lastEnemy[2] = 8;
 lastEnemy[3] = 15;
 lastEnemy[4] = 23;
 lastEnemy[5] = 34;
-
+lastEnemy[6] = 48;
+lastEnemy[7] = 63;
 
 /*******************************************************/
 // setup()
@@ -57,13 +76,15 @@ function setup() {
     console.log("setup: ");
 
     cnv = new Canvas(windowWidth, windowHeight);
-    player = new Sprite(windowWidth/2, windowHeight/2, PLAYER_WIDTH, PLAYER_HEIGHT, 'k');
+    player = new Sprite(windowWidth/2, windowHeight/2, PLAYER_WIDTH, 'k');
     player.color = 'cyan';
  
-    projectile = new Sprite(player.x, player.y, PROJECTILE_WIDTH, PROJECTILE_HEIGHT, 'd');
+    projectile = new Sprite(player.x, player.y, PROJECTILE_WIDTH, 'd');
     projectile.remove();
     projectileGroup = new Group();
-
+    
+    enemyProjectile = new Sprite(0 , 0, PROJECTILE_WIDTH, 'd');
+    enemyProjectile.remove();
     enemyProjectileGroupOne = new Group();
 
 
@@ -81,7 +102,7 @@ function draw() {
     if (screenSelector == "end"){
         endScreen();   
     }
-    console.log(projectileEnemySpawn);
+    console.log(lastEnemy[7]);
 }
    
 function enemyHitPlayer(normalEnemies, player) {
@@ -96,19 +117,29 @@ function projectileHitNormalEnemy(normalEnemies, projectile) {
     lastEnemy[3]--;
     lastEnemy[4]--;
     lastEnemy[5]--;
+    lastEnemy[6]--;
+    lastEnemy[7]--;
 
 }
 function projectile_Hit_Fast_Enemy(fastEnemies, projectile) {
     fastEnemies.remove();
     lastEnemy[4]--;
     lastEnemy[5]--;
+    lastEnemy[6]--;
+    lastEnemy[7]--;
 
 }
-function projectileHitProjectileEnemyOne(projectileEnemyOne, projectile) {
+ function projectileHitProjectileEnemy(projectileEnemyOne, projectile){
     projectileEnemyOne.remove();
-    console.log("hit");
-}
+    enemyProjectileOneCooldown = -10000000000000;
+    lastEnemy[7]--;
+ }
+ function enemyProjectileHitProjectileEnemy(enemyProjectile, projectileEnemyOne){
+    projectileEnemyOne.remove();
+    enemyProjectileOneCooldown = -10000000000000;
+    lastEnemy[7]--;
 
+}
 function playerMechanics(){
     document.addEventListener("keydown", function(event) {
         if (event.code === 'KeyA') {
@@ -154,7 +185,7 @@ function playerSpawn() {
 
 function playerProjectileMechanics() {
     if (mouse.presses() & frameCount > 50) {
-            projectile = new Sprite(player.x, player.y, PROJECTILE_WIDTH, PROJECTILE_HEIGHT, 'k');
+            projectile = new Sprite(player.x, player.y, PROJECTILE_WIDTH, 'k');
             projectile.color = 'cyan';
             projectile.moveTowards(mouseX, mouseY, 0.01);
             projectileGroup.add(projectile);
@@ -174,11 +205,21 @@ function fastEnemySpawn(){
     if (fastEnemyWaveSpawnCounter[2] > 1){
         return(fastEnemySpawn);
     }
+    if (fastEnemyWaveSpawnCounter[3] > 1){
+        return(fastEnemySpawn);
+    }
+    if (fastEnemyWaveSpawnCounter[4] > 1){
+        return(fastEnemySpawn);
+    }
+    if (fastEnemyWaveSpawnCounter[5] > 1){
+        return(fastEnemySpawn);
+    }
     for (i = 0; i < fastEnemyCount; i++) {
-        var randomNumberThree= Math.random() * windowWidth/3;
+        var randomNumberThree = wallRH.x - windowWidth/3;
         var randomNumberFour= Math.random() * windowHeight;
+        console.log(randomNumberThree);
 
-        fastEnemies[i] = new Sprite(randomNumberThree, randomNumberFour, ENEMY_HEIGHT, ENEMY_WIDTH, 'd');
+        fastEnemies[i] = new Sprite(randomNumberThree, randomNumberFour, ENEMY_WIDTH, 'd');
         fastEnemies[i].color = 'orange';
         fastEnemyWaveSpawn = "false";
     }   
@@ -187,47 +228,62 @@ function fastEnemySpawn(){
 function fastEnemyMechanics(){
     for (i = 0; i < fastEnemyCount; i++) {
         fastEnemies[i].moveTowards(player.x, player.y, 0.02)
-        fastEnemies[i].speed = 8;
+        fastEnemies[i].speed = 6;
         fastEnemies[i].collide(projectile, projectile_Hit_Fast_Enemy)
     } 
 }
 
 function projectileEnemyOneSpawn(){
-        if (projectileEnemySpawn > 1){
+        if (projectileEnemyOneSpawnCounter > 1){
         return(projectileEnemyOneSpawn);
     }
-    console.log(projectileEnemySpawn);
-        projectileEnemyOne = new Sprite(25, 25, 25, 'k');
+        projectileEnemyOne = new Sprite(25, 25, 25, 'd');
         projectileEnemyOne.color='yellow';   
-        projectileEnemySpawn++;
+        projectileEnemyOneSpawnCounter++;
 
 }
 
 function projectileEnemyOneMechanics(){
-    enemyProjectileDespawn = enemyProjectileDespawn + 0.02;
-    enemyProjectileOneCooldown = enemyProjectileOneCooldown + 0.02;
-    if (enemyProjectileOneCooldown > 4) {
-        enemyProjectileOneCooldown = enemyProjectileOneCooldown - enemyProjectileOneCooldown;
-        enemyProjectile = new Sprite(projectileEnemyOne.x, projectileEnemyOne.y, PROJECTILE_WIDTH, PROJECTILE_HEIGHT, 'k');
+    enemyProjectileOneDespawn[1] = enemyProjectileOneDespawn[1] + 0.02;
+    enemyProjectileOneCooldown[1] = enemyProjectileOneCooldown[1] + 0.02;
+    if (enemyProjectileOneCooldown[1] > 4) {
+        enemyProjectileOneCooldown[1] = enemyProjectileOneCooldown[1] - enemyProjectileOneCooldown[1];
+        enemyProjectile = new Sprite(projectileEnemyOne.x + 25, projectileEnemyOne.y + 25, PROJECTILE_WIDTH, 'd');
         enemyProjectile.color = 'yellow';
         enemyProjectileGroupOne.add(enemyProjectile)
         enemyProjectile.moveTowards(player.x, player.y, 0.01);
         enemyProjectile.speed = 4;
-        enemyProjectileDespawn = enemyProjectileDespawn - enemyProjectileDespawn;
+        enemyProjectileOneDespawn[1] = enemyProjectileOneDespawn[1] - enemyProjectileOneDespawn[1];
     }
-    if (enemyProjectileDespawn > 4) {
+    projectileEnemyOne.collide(projectile, projectileHitProjectileEnemy)
+    enemyProjectile.collide(projectileEnemyOne, enemyProjectileHitProjectileEnemy)
+
+    if (enemyProjectileOneDespawn[1] > 3.9) {
+        enemyProjectileGroupOne.removeAll();
+    }
+}
+
+function projectileEnemyOneMechanicsTwo(){
+    enemyProjectileOneDespawn[2] = enemyProjectileOneDespawn[2] + 0.02;
+    enemyProjectileOneCooldown[2] = enemyProjectileOneCooldown[2] + 0.02;
+    if (enemyProjectileOneCooldown[2] > 4) {
+        enemyProjectileOneCooldown[2] = enemyProjectileOneCooldown[2] - enemyProjectileOneCooldown[2];
+        enemyProjectile = new Sprite(projectileEnemyOne.x + 25, projectileEnemyOne.y + 25, PROJECTILE_WIDTH, 'd');
+        enemyProjectile.color = 'yellow';
+        enemyProjectileGroupOne.add(enemyProjectile)
+        enemyProjectile.moveTowards(player.x, player.y, 0.01);
+        enemyProjectile.speed = 4;
+        enemyProjectileOneDespawn[2] = enemyProjectileOneDespawn[2] - enemyProjectileOneDespawn[2];
+    }
+    if (enemyProjectileOneDespawn[2] > 3.9) {
         enemyProjectileGroupOne.removeAll();
    }
 }
-
 function normalEnemySpawn(){
     if (normalEnemyWaveSpawnCounter[1] > 1){
         return(normalEnemySpawn);
     }
      if (normalEnemyWaveSpawnCounter[2] > 1){
-        return(normalEnemySpawn);
-    }
-     if (normalEnemyWaveSpawnCounter[3] > 1){
         return(normalEnemySpawn);
     }
      if (normalEnemyWaveSpawnCounter[3] > 1){
@@ -239,13 +295,24 @@ function normalEnemySpawn(){
      if (normalEnemyWaveSpawnCounter[5] > 1){
         return(normalEnemySpawn);
     }
-    
-    
+     if (normalEnemyWaveSpawnCounter[6] > 1){
+        return(normalEnemySpawn);
+    }
+     if (normalEnemyWaveSpawnCounter[7] > 1){
+        return(normalEnemySpawn);
+    }
+     if (normalEnemyWaveSpawnCounter[8] > 1){
+        return(normalEnemySpawn);
+    }
+     if (normalEnemyWaveSpawnCounter[9] > 1){
+        return(normalEnemySpawn);
+    }
+
     for (i = 0; i < normalEnemyCount; i++) {
-        var randomNumber= Math.random() * windowWidth;
+        var randomNumber= Math.random() * windowWidth/3;
         var randomNumberTwo= (Math.random() * windowHeight) ;
 
-        normalEnemies[i] = new Sprite(randomNumber, randomNumberTwo, ENEMY_HEIGHT, ENEMY_WIDTH, 'd');
+        normalEnemies[i] = new Sprite(randomNumber, randomNumberTwo, ENEMY_WIDTH, 'd');
         normalEnemies[i].color = 'red';
         normalEnemyWaveSpawn = "false";
     } 
@@ -254,7 +321,7 @@ function normalEnemySpawn(){
 function normalEnemyMechanics(){
     for (i = 0; i < normalEnemyCount; i++) {
         normalEnemies[i].moveTowards(player.x, player.y, 0.01)
-        normalEnemies[i].speed = 4.2;
+        normalEnemies[i].speed = 3;
         normalEnemies[i].collide(projectile, projectileHitNormalEnemy)
     } 
 }
@@ -275,7 +342,6 @@ function waveOne(){
     fill(255);
     textSize(32);
     text("wave:" + wave, windowWidth/2, 32);
-
 }
    if (lastEnemy[1] == 0){
         wave = wave + 1;
@@ -291,7 +357,7 @@ function waveTwo(){
     playerProjectileMechanics();
     normalEnemyWaveSpawn = "true";
     if (normalEnemyWaveSpawn == "true") {
-        normalEnemyWaveSpawnCounter[1] = normalEnemyWaveSpawnCounter[1] + 1;
+        normalEnemyWaveSpawnCounter[1]++;
         normalEnemyCount = 5;
         normalEnemySpawn();
     }
@@ -313,12 +379,13 @@ function waveThree(){
     lastEnemy[2] = 10;
     playerMechanics();
     playerProjectileMechanics();
-    normalEnemyWaveSpawnCounter[1] = 0;
+    normalEnemyWaveSpawnCounter[1] = 1;
     normalEnemyWaveSpawn = "true";
     if (normalEnemyWaveSpawn == "true") {
-        normalEnemyWaveSpawnCounter[2] = normalEnemyWaveSpawnCounter[2] + 1;
+        normalEnemyWaveSpawnCounter[2]++;
         normalEnemyCount = 7;
         normalEnemySpawn();
+        
     }
     normalEnemyMechanics();
     fill(255);
@@ -344,7 +411,7 @@ function waveFour(){
     playerMechanics();
     normalEnemyWaveSpawn = "true";
     if (normalEnemyWaveSpawn == "true") {
-        normalEnemyWaveSpawnCounter[3] = normalEnemyWaveSpawnCounter[3] + 1;
+        normalEnemyWaveSpawnCounter[3]++;
         normalEnemyCount = 7;
         normalEnemySpawn();
     }
@@ -382,14 +449,14 @@ function waveFive(){
     playerMechanics();
     normalEnemyWaveSpawn = "true";
     if (normalEnemyWaveSpawn == "true") {
-        normalEnemyWaveSpawnCounter[4] = normalEnemyWaveSpawnCounter[4] + 1;
+        normalEnemyWaveSpawnCounter[4]++;
         normalEnemyCount = 9;
         normalEnemySpawn();
     }
     normalEnemyMechanics();
     fastEnemyWaveSpawn = "true";
     if (fastEnemyWaveSpawn == "true"){
-    fastEnemyWaveSpawnCounter[1] = fastEnemyWaveSpawnCounter[1] + 1;
+    fastEnemyWaveSpawnCounter[1]++;
     fastEnemyCount = 2;
     fastEnemySpawn();
     }
@@ -426,21 +493,71 @@ function waveSix(){
     playerMechanics();
     normalEnemyWaveSpawn = "true";
     if (normalEnemyWaveSpawn == "true") {
-        normalEnemyWaveSpawnCounter[5] = normalEnemyWaveSpawnCounter[5] + 1;
-        normalEnemyCount = 9;
+        normalEnemyWaveSpawnCounter[5]++;
+        normalEnemyCount = 10;
         normalEnemySpawn();
     }
     normalEnemyMechanics();
     fastEnemyWaveSpawn = "true";
     if (fastEnemyWaveSpawn == "true"){
-    fastEnemyWaveSpawnCounter[2] = fastEnemyWaveSpawnCounter[2] + 1;
-    fastEnemyCount = 2;
+    fastEnemyWaveSpawnCounter[2]++;
+    fastEnemyCount = 4;
+    fastEnemySpawn();
+    }
+    fastEnemyMechanics();
+
+    
+    fill(255);
+    textSize(32);
+    text("wave:" + wave, windowWidth/2, 32);
+
+}
+    if (lastEnemy[6] == 0){
+        wave = wave + 1;
+    } 
+    if (wave == 7){
+        waveSeven();
+    } 
+
+
+function waveSeven(){
+    background('black'); 
+    lastEnemy[1] = 3;
+    lastEnemy[2] = 10;
+    lastEnemy[3] = 17;
+    lastEnemy[4] = 23;
+    lastEnemy[5] = 34;
+    lastEnemy[6] = 48;
+
+    normalEnemyWaveSpawnCounter[1] = 0;
+    normalEnemyWaveSpawnCounter[2] = 0;
+    normalEnemyWaveSpawnCounter[3] = 0;
+    normalEnemyWaveSpawnCounter[4] = 0;
+    normalEnemyWaveSpawnCounter[5] = 0;
+
+    fastEnemyWaveSpawnCounter[1] = 0;
+    fastEnemyWaveSpawnCounter[2] = 0;
+
+    playerProjectileMechanics();
+    playerMechanics();
+    normalEnemyWaveSpawn = "true";
+    if (normalEnemyWaveSpawn == "true") {
+        normalEnemyWaveSpawnCounter[6]++;
+        normalEnemyCount = 10;
+        normalEnemySpawn();
+    }
+    normalEnemyMechanics();
+    fastEnemyWaveSpawn = "true";
+    if (fastEnemyWaveSpawn == "true"){
+    fastEnemyWaveSpawnCounter[3]++;
+    fastEnemyCount = 4;
     fastEnemySpawn();
     }
     fastEnemyMechanics();
     projectileEnemyOneSpawn();
     projectileEnemyOneMechanics();
-   fill(255);
+    
+    fill(255);
     textSize(32);
     text("wave:" + wave, windowWidth/2, 32);
 
