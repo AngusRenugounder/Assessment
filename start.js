080b1f0eadf778c650bfbab35c69adfcba2ee1ba
AngusRenugounder <161382565+AngusRenugounder@users.noreply.github.com>
2024-04-03 10:09:54 +1300

    Update start.js
    added start screen elements

--- start.js
+++ start.js
@@ -1,1 +1,53 @@
+/*******************************************************/
+// P5.play: Assessment
+// Move sprite based on keyboard input
+// Written by Angus Renugounder
+/*******************************************************/
 
+/*******************************************************/
+// preload()
+// Called by P5 before setup
+/*******************************************************/
+function preload() {
+    gameTitleImage = loadImage('gameTitle.png');
+
+}
+
+/*******************************************************/
+// setup()
+/*******************************************************/
+function setup() {
+    console.log("setup: ");
+    gameTitle = new Sprite(windowWidth/2, windowHeight/3, 100, 50, 'd');
+    gameTitle.addImage(gameTitleImage);
+    gameTitleImage.resize(400, 200);
+
+    cnv = new Canvas(windowWidth, windowHeight);
+     
+    start = new Sprite(windowWidth/2, windowHeight/1.75, 100, 50, 'd');
+    start.color = 'cyan';
+    start.text = "Start";
+    
+    leaderboard = new Sprite(windowWidth/2, windowHeight/1.5, 100, 50, 'd');
+    leaderboard.color = 'cyan';
+    leaderboard.text = "Leaderboard";
+}
+
+/*******************************************************/
+// draw()
+/*******************************************************/
+function draw() {
+    background('black');
+    
+    if(start.mouse.presses()){
+        window.location = "game.html";
+    }
+    if(leaderboard.mouse.presses()){
+        window.location = "leaderboard.html";
+    }
+}
+
+
+/*******************************************************/
+//  END OF APP
+/*******************************************************/
